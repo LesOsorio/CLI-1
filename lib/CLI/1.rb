37cdc981e0431989fb931a2ebc3a3f3be300f869
Leslie Osorio <lesosorio17@gmail.com>
2019-01-16 21:37:37 +0000

    first commit

---
+++ lib/CLI/1.rb
@@ -0,0 +1,8 @@
+require "CLI/1/version"
+
+module CLI
+  module 1
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/CLI/1/version.rb
@@ -0,0 +1,5 @@
+module CLI
+  module 1
+    VERSION = "0.1.0"
+  end
+end
